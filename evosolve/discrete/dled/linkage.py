92845597dd5b4d1129ebdb25f0642fe13bea4e09
Piotr Rarus <piotr.rarus@softwarehut.com>
2021-06-01 16:40:59 +0200

    Added DLED

---
+++ evosolve/discrete/dled/linkage.py
@@ -0,0 +1,66 @@
+from typing import List, Tuple
+
+import numpy as np
+from evobench import Benchmark, Solution
+
+
+def get_scraps_for_solution(
+    solution: Solution,
+    benchmark: Benchmark
+) -> Tuple[np.ndarray, np.ndarray]:
+
+    scraps: List[np.ndarray] = []
+    interactions: List[np.ndarray] = []
+
+    for gene_index in range(solution.genome.size):
+        gene_scrap, gene_interactions = get_scrap(solution, gene_index, benchmark)
+        scraps.append(gene_scrap)
+        interactions.append(gene_interactions)
+
+    scraps = np.vstack(scraps)
+    interactions = np.vstack(interactions)
+    return scraps, interactions
+
+
+# def get_scraps_for_gene(
+#     gene_index: int,
+#     solutions: List[Solution],
+# ) -> Tuple[np.ndarray, np.ndarray]:
+#     scraps: List[np.ndarray] = []
+#     interactions: List[np.ndarray] = []
+
+#     for solution in solutions:
+#         gene_scrap, gene_interactions = get_scrap(solution, gene_index)
+#         scraps.append(gene_scrap)
+#         interactions.append(gene_interactions)
+
+#     scraps = np.vstack(scraps)
+#     interactions = np.vstack(interactions)
+#     return scraps, interactions
+
+
+def get_scrap(
+    solution: Solution,
+    gene_index: int,
+    benchmark: Benchmark
+) -> Tuple[np.ndarray, np.ndarray]:
+    raise NotImplementedError()
+
+    # loci_order = fihc.get_loci_order()
+    # fihc_solution = fihc(solution, loci_order)
+
+    # modified = solution.genome.copy()
+    # modified[gene_index] = not modified[gene_index]
+    # modified = Solution(modified)
+    # fihc_modified = fihc(modified, loci_order)
+
+    # interactions = np.abs(fihc_solution.genome - fihc_modified.genome)
+    # mask = np.ones(fihc.benchmark.genome_size, dtype=bool)
+    # mask[gene_index] = False
+    # interactions = interactions[mask]
+    # scrap = np.argsort(interactions)
+    # scrap = np.flip(scrap)
+    # interactions = interactions[scrap]
+    # scrap[scrap >= gene_index] += 1
+
+    # return scrap, interactions
